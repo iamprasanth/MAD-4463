b739a886c52d7854f4ebae339580fab81394c3b6
iamprasanth <prasanth1996@gmail.com>
2021-10-08 08:10:52 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+/**
+ * @author prasanth
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator(100, 9);
+        System.out.println("Final value " + (new Calculator(1, 100)).addNumbers());
+	}
+}
+
+class Calculator {
+	int startValue;
+	int endValue;
+	
+	public Calculator(int _startValue, int _endValue) {
+		// _ for function local variables, this for instance variables
+		this.startValue = _startValue;
+		this.endValue = _endValue;
+	}
+    
+	int addNumbers() {
+    	// static functions wont have access to instance variable
+    	for(int counter = this.startValue; counter <= this.endValue; counter += this.stepValue) {
+    		this.finalValue = this.finalValue + counter;
+    	}
+    	
+    	return this.startValue + this.endValue;
+    }
+}
